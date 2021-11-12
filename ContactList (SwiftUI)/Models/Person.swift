43b92d0fe5875c236708416ebc53aa6979d98a1a
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-12 23:56:05 +0500

    Person done.

--- ContactList (SwiftUI)/Models/Person.swift
+++ ContactList (SwiftUI)/Models/Person.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+struct Person {
+    let name: String
+    let secondName: String
+    let numberPhone: String
+    let email: String
+    
+    var fullName: String {
+        "\(name) \(secondName)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataManager.shared.realNames.shuffled()
+        let secondsNames = DataManager.shared.realSecondNames.shuffled()
+        let phoneNumbers = DataManager.shared.realNumbersPhones.shuffled()
+        let emails = DataManager.shared.realEmails.shuffled()
+        
+        for index in 0..<names.count {
+            let person = Person(
+                name: names[index],
+                secondName: secondsNames[index],
+                numberPhone: phoneNumbers[index],
+                email: emails[index]
+            )
+            persons.append(person)
+        }
+        return persons
+    }
+}
+
+enum Contacts: String {
+    case phone = "phone" //название кнопочек, которые используем на втором экране
+    case email = "tray" //название кнопочек
+}
