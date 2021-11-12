a5dfbdc644e0b58065f311a9238d0241cee130e3
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-12 23:12:39 +0500

    Initial Commit

---
+++ ContactList (SwiftUI)/ContactList__SwiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  ContactList__SwiftUI_App.swift
+//  ContactList (SwiftUI)
+//
+//  Created by iMac on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContactList__SwiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
