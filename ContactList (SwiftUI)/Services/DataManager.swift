a78b50f135d075dc1b835706313257aaebbe9254
Artem Pavlov <pavlov.tema@gmail.com>
2021-11-12 23:46:40 +0500

    DataManager done.

--- ContactList (SwiftUI)/Services/DataManager.swift
+++ ContactList (SwiftUI)/Services/DataManager.swift
@@ -2,7 +2,81 @@
 //  DataManager.swift
 //  ContactList (SwiftUI)
 //
-//  Created by iMac on 12.11.2021.
+//  Created by Pavlov Artem on 12.11.2021.
 //
 
 import Foundation
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    let realNames = [
+        "John",
+        "Steven",
+        "Misha",
+        "Boris",
+        "Melanie",
+        "Bred",
+        "Ted",
+        "Sharon",
+        "Megan",
+        "Sarah",
+        "Misha",
+        "Georg",
+        "Nancy",
+        "Sam"
+    ]
+    
+    let realSecondNames = [
+        "Smith",
+        "Karter",
+        "White",
+        "Brown",
+        "Willis",
+        "Trump",
+        "Pitt",
+        "Dancher",
+        "Fox",
+        "Catcher",
+        "Rork",
+        "Pitcher",
+        "Tarantino",
+        "Hottkin"
+    ]
+    
+    let realNumbersPhones = [
+        "8-900-600-80-80",
+        "8-900-555-55-40",
+        "8-800-100-55-55",
+        "8-999-640-80-10",
+        "8-950-600-40-44",
+        "8-955-440-42-22",
+        "8-900-220-20-20",
+        "8-800-200-33-33",
+        "8-800-400-43-43",
+        "8-800-400-55-35",
+        "8-800-800-88-88",
+        "8-800-300-23-22",
+        "8-770-505-35-33",
+        "8-300-300-33-03",
+    ]
+    
+    let realEmails = [
+        "222@swiftbook.ru",
+        "tit@mail.ru",
+        "pholik@mail.ru",
+        "dodds@icloud.com",
+        "my@gmail.com",
+        "biggi@rambler.ru",
+        "cat@swiftbook.ru",
+        "works@mail.ru",
+        "Gogg@gmail.com",
+        "first11@icloud.com",
+        "kuku@mail.ru",
+        "samsam@gmail.com",
+        "ozoo@mail.ru",
+        "qojje3@mail.ru"
+    ]
+    
+    private init() {}  /
+}
